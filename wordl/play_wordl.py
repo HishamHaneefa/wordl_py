91a76cc906ed65f8d080db71f70fbbfbfbf89217
HishamHaneefa <haneefa.hishams@gmail.com>
2022-03-19 16:48:07 +0530

    game initials

---
+++ wordl/play_wordl.py
@@ -0,0 +1,8 @@
+from wordl.wordle import Wordle
+
+def main():
+    print("hello world")
+    Wordl = Wordle # creating instance 
+
+if __name__ == "__main__":
+    main()
---
+++ wordl/wordle.py
@@ -0,0 +1,8 @@
+from mimetypes import init
+
+
+class Wordle:
+
+    #  constructor
+    def __init__(self):
+        pass
